d26d6b0f0d1841e19b80ad253113a9df3b5d2cda
tommyd206 <161645124+tommyd206@users.noreply.github.com>
2025-01-31 07:31:18 +0000

    Deploying to gh-pages from @ tommyd206/tommyd206.github.io@8b18cf1bdbdb7bd45487dc7ae0e73290f6ffb040 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,13 +89,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/tommy-dunn-a13266b8", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
